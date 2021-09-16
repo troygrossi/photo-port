350f951b1f7531a3cb6c01a7432f38ad48f6bb52
Troy Grossi <ttroygrossit@gmail.com>
2021-09-16 13:03:06 -0700

    added about tests

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import About from '..';
+
+afterEach(cleanup);
+
+describe('About component', () => {
+    // First Test
+    test('renders', () => {
+      render(<About />);
+    });
+  
+    // Second Test
+    test('matches snapshot DOM node structure', () => {
+        // render About
+        const { asFragment } = render(<About />);
+        expect(asFragment()).toMatchSnapshot();
+      });
+  });
